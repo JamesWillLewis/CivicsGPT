9d56d9de2b6e697e79cdd80f38203bb9e52f3470
James Lewis <james.will.lewis@gmail.com>
2023-06-01 09:32:11 -0400

    Add chatbox question-and-answer logic

--- src/civicsgpt/main.py
+++ src/civicsgpt/main.py
@@ -1,17 +1,17 @@
 import os
 import sys
-
 import openai
 
 GPT_MODEL = "gpt-3.5-turbo"
 MAX_TOKENS_DEFAULT = 16
 TEMPERATURE_DEFAULT = 0.2
-DEFAULT_PROMPT = "I forgot to ask something."
+INITIAL_PROMPT = "Ask me a question"
+NEXT_PROMPT = "Ask me another question"
+SYSTEM_MESSAGE = "You are asking US Civics questions, and checking the answer after the user replies. After the user responds, ask another question."
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-prompt = sys.argv[1] if len(sys.argv) > 1 else DEFAULT_PROMPT
-
+prompt = sys.argv[1] if len(sys.argv) > 1 else INITIAL_PROMPT
 completion_args = {}
 if len(sys.argv) > 2:
     completion_args['temperature'] = float(sys.argv[2])
@@ -20,11 +20,29 @@ if len(sys.argv) > 3:
 
 print(f"You asked: '{prompt}' with args: {completion_args}")
 print(f'Using model {GPT_MODEL}')
+print(f'Type q to quit')
 
-response = openai.ChatCompletion.create(
-    model=GPT_MODEL,
-    messages=[
-        {"role": "user", "content": prompt},
-    ],
-    **completion_args)
-print(f'GPT says: {response}')
+current_prompt = INITIAL_PROMPT
+
+messages = [
+    {"role": "system", "content": SYSTEM_MESSAGE},
+    {"role": "user", "content": current_prompt},
+]
+
+while True:
+
+    question_response = openai.ChatCompletion.create(
+        model=GPT_MODEL,
+        messages=messages,
+        **completion_args)
+
+    gpt_question = question_response['choices'][0]['message']['content']
+    print(f'GPT says: {gpt_question}')
+    answer = input("Your answer: ")
+    if answer == 'q':
+        break
+
+    messages.append({"role": "assistant", "content": gpt_question})
+    messages.append({"role": "user", "content": answer})
+
+
